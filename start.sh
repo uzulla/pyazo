a22aaf1f6cee8d13284ec11a8ef855f171833659
Junichi Ishida <uzulla@himitsukichi.com>
2013-06-19 20:11:00 +0900

    Update start.sh
    change to popular path

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#export IM_CONVERT_PATH="/Users/zishida/opt/ym/bin/convert"
-export IM_CONVERT_PATH="/opt/local/bin/convert"
-export FFMPEG_PATH="/opt/local/bin/ffmpeg"
+#export IM_CONVERT_PATH="/usr/bin/convert"
+#export FFMPEG_PATH="/usr/bin/ffmpeg"
 plackup -s Starman -l :5000 app.pl
